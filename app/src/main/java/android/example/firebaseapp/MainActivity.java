44bb27f346ce37900b168cc94eba2061a72d75fc
root <blaj.david1@gmail.com>
2020-08-10 21:26:13 +0300

    - finished 4'th video - created text field and added it's value in the database(MainActivity.txtEdit)

--- app/src/main/java/android/example/firebaseapp/MainActivity.java
+++ app/src/main/java/android/example/firebaseapp/MainActivity.java
@@ -6,13 +6,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnLogout;
+    private EditText txtEdit;
+    private Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnLogout = findViewById(R.id.logout);
+        txtEdit = findViewById(R.id.edit);
+        btnAdd = findViewById(R.id.add);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +37,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, StartActivity.class));
             }
         });
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = txtEdit.getText().toString();
+                if(name.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "No data entered", Toast.LENGTH_LONG).show();
+                } else {
+                    FirebaseDatabase.getInstance().getReference().child("catalysts").push().child("name").setValue(name);
+                }
+            }
+        });
+
     }
 }
