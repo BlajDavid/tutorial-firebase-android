89af008e43650f9f377d1c5b0c96350365ede26f
root <blaj.david1@gmail.com>
2020-08-11 20:40:14 +0300

    - finished 5'th video - added ListView to MainActivity and added Firebase realtime database

---
+++ app/src/main/java/android/example/firebaseapp/Information.java
@@ -0,0 +1,30 @@
+package android.example.firebaseapp;
+
+public class Information {
+    private String name;
+    private String email;
+
+    public Information() {
+    }
+
+    public Information(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- app/src/main/java/android/example/firebaseapp/MainActivity.java
+++ app/src/main/java/android/example/firebaseapp/MainActivity.java
@@ -1,24 +1,32 @@
 package android.example.firebaseapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnLogout;
     private EditText txtEdit;
     private Button btnAdd;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         btnLogout = findViewById(R.id.logout);
         txtEdit = findViewById(R.id.edit);
         btnAdd = findViewById(R.id.add);
+        listView = findViewById(R.id.listView);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,13 +51,35 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String name = txtEdit.getText().toString();
-                if(name.isEmpty()) {
+                if (name.isEmpty()) {
                     Toast.makeText(MainActivity.this, "No data entered", Toast.LENGTH_LONG).show();
                 } else {
-                    FirebaseDatabase.getInstance().getReference().child("catalysts").push().child("name").setValue(name);
+                    FirebaseDatabase.getInstance().getReference().child("languages").child("name").setValue(name);
                 }
             }
         });
 
+        final ArrayList<String> list = new ArrayList<>();
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_item, list);
+        listView.setAdapter(adapter);
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Information");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for (DataSnapshot s : snapshot.getChildren()) {
+                    Information info = s.getValue(Information.class);
+                    assert info != null;
+                    String item = info.getName() + " : " + info.getEmail();
+                    list.add(item);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
