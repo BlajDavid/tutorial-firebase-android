750fb2c7ff9e8a7784f73eecb243c0007722dec8
root <blaj.david1@gmail.com>
2020-08-27 18:58:27 +0300

    - am creeat un nou fragment: PostDetailFragment - am folosit photo_item pentru a deschide imaginile de pe profilul unui user - am setat ca atunci cand apas pe username, sau pe poza de profil, ma redirectioneaza catre profilul user-ului respectiv - atunci cand dau click pe o poza in HomeFragment mi se deschide poza - finished video 32

--- app/src/main/java/android/example/firebaseapp/adapter/PhotoAdapter.java
+++ app/src/main/java/android/example/firebaseapp/adapter/PhotoAdapter.java
@@ -2,6 +2,7 @@ package android.example.firebaseapp.adapter;
 
 import android.content.Context;
 import android.example.firebaseapp.R;
+import android.example.firebaseapp.fragments.PostDetailFragment;
 import android.example.firebaseapp.model.Post;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
@@ -36,9 +38,21 @@ public class PhotoAdapter extends RecyclerView.Adapter<PhotoAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-        Post post = posts.get(position);
+        final Post post = posts.get(position);
 
         Picasso.get().load(post.getImageUrl()).placeholder(R.mipmap.ic_launcher).into(holder.postImage);
+
+        holder.postImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mContext.getSharedPreferences("SharedPreferencesPhoto", Context.MODE_PRIVATE).edit().putString("postId", post.getPostId()).apply();
+
+                // call PostDetailFragment
+                ((FragmentActivity)mContext).getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new PostDetailFragment())
+                        .commit();
+            }
+        });
     }
 
     @Override
